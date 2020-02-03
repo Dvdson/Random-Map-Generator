4ac35612b9ce43f16a4d036efcdc8ba7b213b268
Davidson <dvdsoncomp@gmail.com>
2020-02-02 23:39:58 -0300

    Criado porcentagem de culling e controle de forma horizontal

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -6,6 +6,8 @@ var tile_size = 32
 var num_rooms = 50
 var min_size = 4
 var max_size = 10
+var hspread = 400
+var cull = 0.5 # Needs to be a percent
 
 func _ready():
 	randomize()
@@ -13,10 +15,26 @@ func _ready():
 	
 func make_rooms():
 	for i in range(num_rooms):
-		var pos = Vector2(0,0)
+		var pos = Vector2(rand_range(-hspread, hspread),0)
 		var r = Room.instance()
 		var w = min_size + randi() % (max_size - min_size)
 		var h = min_size + randi() % (max_size - min_size)
 		r.make_room(pos, Vector2(w, h) * tile_size)
 		$Rooms.add_child(r)
-		
+	yield(get_tree().create_timer(1.1), "timeout")
+func _draw():
+	for room in $Rooms.get_children():
+		draw_rect(Rect2(
+			room.position - room.size, room.size * 2),
+			Color(32, 228, 0),
+			false
+		)
+
+func _process(delta):
+	update()
+	
+func _input(event):
+	if event.is_action_pressed('ui_select'):
+		for n in $Rooms.get_children():
+			n.queue_free()
+		make_rooms()
